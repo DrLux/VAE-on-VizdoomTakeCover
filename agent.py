4f0f5c53f375c02e496311e7b52ffc12306bb2b2
AddLux <sorrentino.luca@outlook.com>
2019-10-30 15:45:14 +0100

    changed classes and add documentation

--- agent.py
+++ agent.py
@@ -4,16 +4,23 @@ tfd = tf.contrib.distributions
 import dataset #my file
 import vae #my file
 
+#Hyperparameters
+
+#Dataset
+env_id = 'Freeway-v0'
+dataset_size = 1000
+frame_shape = [64,64]
+batch_size = 10
+
+#VAE
 latent_size = 8
 learning_rate = 0.001
-train_epochs = 100
+train_epochs = 10
 
+num_batches = int(dataset_size/batch_size)
+print("*************num_batches: ", num_batches)
 
-dataset = dataset.Dataset('Freeway-v0',10000,[64,64],10)
-vae = vae.VAE(latent_size,learning_rate,train_epochs)
-
-#dataset.create_dataset()
-#dataset.test_dataset()
-
+dataset = dataset.Dataset(env_id,dataset_size,frame_shape,batch_size)
+vae = vae.VAE(latent_size,learning_rate,train_epochs,frame_shape,batch_size,num_batches)
 vae.train_vae(dataset)
-
+#vae.sample_random_image()
--- dataset.py
+++ dataset.py
@@ -8,48 +8,55 @@ tfd = tf.contrib.distributions
 import matplotlib.pyplot as plt 
 
 class Dataset(object):
-    def __init__(self, env_id,db_size,frame_shape,batch_size):
-        self.env_id = env_id # Name env
-        self.size_database = db_size # number of frames to collect
+    
+    def __init__(self,env_id,db_size,frame_shape,batch_size):
+        self.env_id = env_id # Name env on open ai gym
+        self.size_dataset = db_size # number of frames to collect
         self.batch_size = batch_size # batch size
-        self.frame_shape = frame_shape  # 64,64
+        self.frame_shape = frame_shape  # final shape for each frame [64,64]
 
-        self.env = gym.make('Freeway-v0')
-        self.frames = []
+        self.env = gym.make(env_id)
+        self.frames = [] # list of frames to collect
 
         self.iterator = None
         self.input_batch = None
 
         self.create_dataset()
         
+    
     def create_dataset(self):
         obs = self.env.reset()
-        for itr in range(self.size_database):
+        for itr in range(self.size_dataset):
             obs,rew,done,_ = self.env.step(self.env.action_space.sample()) # take a random action
-            obs = obs[25:195, 10:, :].astype(np.float)/255.0
-            obs = ((1.0 - obs) * 255).round().astype(np.uint8)
-            obs = np.array(PilImage.fromarray(obs,'RGB').resize((self.frame_shape[0],self.frame_shape[1])))
+            obs = obs[25:195, 10:, :]# crop image
+            obs = np.array(PilImage.fromarray(obs,'RGB').resize((self.frame_shape[0],self.frame_shape[1]))) #resize img
             self.frames.append(obs)
         self.env.close()
         self.frames = np.stack(self.frames)
-        print("######################## DATABASE CREATO ########################")
-
+       
+        #turn list of frames to tensorflow dataset type
         with tf.variable_scope("DataPipe"):
+            #create a dataset whose elements are the frames collected
             dataset = tf.data.Dataset.from_tensor_slices(self.frames)
+            #Convert each image of dataset to dtype
             dataset = dataset.map(lambda x: tf.image.convert_image_dtype([x], dtype=tf.float32))
+            #split dataset in batches
             dataset = dataset.batch(batch_size=self.batch_size).prefetch(self.batch_size)
-
+            
+            # create an iterator over the dataset
             self.iterator = dataset.make_initializable_iterator()
+            #get a batch of data
             self.input_batch = self.iterator.get_next()
+            # reshape from (?, 1, frame_shape[0], frame_shape[1], 3) to (?, , frame_shape[0], frame_shape[1], 3)
             self.input_batch = tf.reshape(self.input_batch, shape=[-1, self.frame_shape[0], self.frame_shape[1], 3])
     
 
+    
 
     def test_dataset(self):
         init_vars = [tf.local_variables_initializer(), tf.global_variables_initializer()]
         with tf.Session() as sess:
             sess.run([init_vars, self.iterator.initializer]) # Initialize variables and the iterator
-
             while 1:    # Iterate until we get out of range error!
                 try:
                     batch = sess.run(self.input_batch)
@@ -59,8 +66,8 @@ class Dataset(object):
                 except tf.errors.OutOfRangeError:  # This exception is triggered when all batches are iterated
                     print('All batches have been iterated!')
                     break
-
-
+    
+    
     def get_single_batch(self):
         init_vars = [tf.local_variables_initializer(), tf.global_variables_initializer()]
         with tf.Session() as sess:
@@ -70,5 +77,3 @@ class Dataset(object):
             except tf.errors.OutOfRangeError:  # This exception is triggered when all batches are iterated
                 print('All batches have been iterated!')
         return batch
-
-    
